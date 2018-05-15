80a9aca42f28aa26e3a467f526572df62763dbfa
君島克弥 <K.Kimishima123@gmail.com>
2018-05-15 21:55:25 +0900

    <追加>railsのルーティングの設定

--- start_rails/app/controllers/application_controller.rb
+++ start_rails/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    text = 'hello'
+    # 画面に文字を出力する
+    render plain: text
+  end
 end
--- start_rails/config/routes.rb
+++ start_rails/config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  # resourcesはrestの一通りのルーティング作製
+  # onlyでホワイトリスト的に追加使いできる
+  # 例 resources :articles, only: [:index, :show]
+
   resources :articles
   resources :users
   get 'home/index'
+
+  # この様にルーティングもデキる
+  # get '/help', to: 'static_pages#help'
+
+  get '/hello', to: 'application#hello'
+
+  # ルートの名前を変える際は
+  # get '/hello', to: 'application#hello' as foo
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
