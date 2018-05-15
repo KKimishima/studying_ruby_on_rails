1754054966e7553565d0bfc4177b1b5a1f17cabc
君島克弥 <K.Kimishima123@gmail.com>
2018-05-15 22:00:55 +0900

    <追加>railsのhttpパラメータの扱い

--- start_rails/app/controllers/application_controller.rb
+++ start_rails/app/controllers/application_controller.rb
@@ -2,8 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    text = 'hello'
+    # text = 'hello'
+    text = "params: #{params}"
     # 画面に文字を出力する
+    # /hello?hoge=hogeにすると
+    # params: {"hoge"=>"hoge", "controller"=>"application", "action"=>"hello"}
     render plain: text
   end
 end
