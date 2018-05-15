15487152ecff76584c5d23fcf6e2f994a6c5beff
君島克弥 <K.Kimishima123@gmail.com>
2018-05-15 23:15:49 +0900

    <追加>railsのエラー表示

--- start_rails/app/controllers/application_controller.rb
+++ start_rails/app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # アクティブレコードで検索失敗した場合
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  # 400系エラー
+  # publicの対応するhtmlに送られる
+  # 500系エラーは無理にハンドリングせず,
+  # 自動的に対応するhtmlに送る
+  def not_found
+    render "errors/404", status: 404
+  end
 
   def hello
     # text = 'hello'
