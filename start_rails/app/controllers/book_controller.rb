cb8ad22a7bed85d3c1d05e9e5b8e29557bcca3fe
君島克弥 <K.Kimishima123@gmail.com>
2018-05-17 22:18:45 +0900

    <追加>viewHelper追加

--- start_rails/app/controllers/book_controller.rb
+++ start_rails/app/controllers/book_controller.rb
@@ -1,6 +1,8 @@
 class BookController < ApplicationController
   layout 'book'
   def index
+    # flashメッセージの設定
+    flash[:notice] = '一度だけ表示れるメッセージ'
 
   end
 end
--- start_rails/app/helpers/book_helper.rb
+++ start_rails/app/helpers/book_helper.rb
@@ -1,2 +1,5 @@
 module BookHelper
+  def demo_helper(demo)
+    demo + "デモヘルパー"
+  end
 end
