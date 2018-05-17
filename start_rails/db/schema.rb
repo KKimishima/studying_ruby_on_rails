6be1e41e2141a122b29c1c9c47e1117cddb68d84
君島克弥 <K.Kimishima123@gmail.com>
2018-05-17 22:30:43 +0900

    bin/rails g model user name:string:index email:string bin/rails db:migrate

--- start_rails/db/schema.rb
+++ start_rails/db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_123342) do
+ActiveRecord::Schema.define(version: 2018_05_17_132714) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "tilte"
     t.string "contents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name"
   end
 
 end
