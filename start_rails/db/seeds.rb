9e14abbb97e7de47743b197bb8446d06219fd896
君島克弥 <K.Kimishima123@gmail.com>
2018-05-19 13:04:14 +0900

    <追加>seedの作り方

--- start_rails/db/seeds.rb
+++ start_rails/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 初期データを挿入
+3.times do |id|
+  User.create!(name: "User-#{id}", email: "hogehoge#{id}@hoge")
+end
