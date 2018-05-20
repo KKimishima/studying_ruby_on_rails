3a0a06750f92ddc4e3a7030fc11923b9669176d4
君島克弥 <K.Kimishima123@gmail.com>
2018-05-20 14:46:43 +0900

    <追加>ユーザと投稿コンテンツを紐づけ
    bin/rails g migration add_user_ref_to_articles user:references

--- rdb_set_rails/db/schema.rb
+++ rdb_set_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_053646) do
+ActiveRecord::Schema.define(version: 2018_05_20_054158) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_05_20_053646) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 2018_05_20_053646) do
     t.index ["name"], name: "index_users_on_name"
   end
 
+  add_foreign_key "articles", "users"
 end
