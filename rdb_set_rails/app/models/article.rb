b6ecfa958a988a8cf47162f34388e0eda1dc7e72
君島克弥 <K.Kimishima123@gmail.com>
2018-05-20 15:36:07 +0900

    <追加> 中間テーブルをアクティブレコードに教える

--- rdb_set_rails/app/models/article.rb
+++ rdb_set_rails/app/models/article.rb
@@ -3,4 +3,9 @@ class Article < ApplicationRecord
   belongs_to :user
   # Articleが削除された場合、関連したcommentを削除する
   has_many :comment, dependent: :destroy
+
+  # 中間テーブルの設定
+  # Aritcleはarticles_tagを通してtagを所有する
+  has_many :article_tag
+  has_many :tag, through: :article_tag
 end
--- rdb_set_rails/app/models/tag.rb
+++ rdb_set_rails/app/models/tag.rb
@@ -1,2 +1,4 @@
 class Tag < ApplicationRecord
+  has_many :article_tag
+  has_many :article, through: :article_tag
 end
