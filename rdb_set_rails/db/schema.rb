745af800fe91e93a9d886b0b5038a0eda18e2680
君島克弥 <K.Kimishima123@gmail.com>
2018-05-20 14:38:18 +0900

    <追加>tagテーブル追加
    bin/rails g model tag name:string

--- rdb_set_rails/db/schema.rb
+++ rdb_set_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_053323) do
+ActiveRecord::Schema.define(version: 2018_05_20_053646) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2018_05_20_053323) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
