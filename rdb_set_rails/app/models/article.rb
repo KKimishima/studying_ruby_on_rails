2ead9f77b1aca0fd88dd221e65208e42e95c7b91
君島克弥 <K.Kimishima123@gmail.com>
2018-05-20 15:13:46 +0900

    <追加>アクティブレコードにテーブル関連設定を教える

--- rdb_set_rails/app/models/article.rb
+++ rdb_set_rails/app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  # 参照先をアクティブレコードに教える
+  belongs_to :user
 end
--- rdb_set_rails/app/models/user.rb
+++ rdb_set_rails/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  # has_manyで関連することを設定する
+  # dependent :destroy はuserを削除した際に articleをまとめて削除するオプション
+  has_many :article, dependent: :destroy
 end
