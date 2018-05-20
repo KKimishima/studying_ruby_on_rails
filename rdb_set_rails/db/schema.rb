668487216e9e265175f23d8097c05f9d4eeb158d
君島克弥 <K.Kimishima123@gmail.com>
2018-05-20 15:16:56 +0900

    <追加>bin/rails g migration add_user_ref_to_comments user:references
    ユーザテーブルとコメントを結合

--- rdb_set_rails/db/schema.rb
+++ rdb_set_rails/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_20_054158) do
+ActiveRecord::Schema.define(version: 2018_05_20_061540) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_05_20_054158) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2018_05_20_054158) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "comments", "users"
 end
