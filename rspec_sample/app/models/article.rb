6d4ec93a07830c706b36362aa13fea8d9fa08212
君島克弥 <K.Kimishima123@gmail.com>
2018-05-24 22:31:48 +0900

    <追加>rspecの振る舞いについて

--- rspec_sample/app/models/article.rb
+++ rspec_sample/app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ApplicationRecord
+  # enum機能でステータスを解決できようにしておく
+  # draft: 下書き
+  # published: 記事を公開状態
+  enum status: {draft: 0, published: 1}
+
+  # 20文字以上の記事タイトルは省略
+  def abbreviated_title
+    title.size >= 20  ? "#{title.slice(0,19)}..." : title
+  end
+
+  # 記事を公開する
+  def publish
+    return if self.published?
+    update({status: Article.statuses['published'], published_at: Time.current})
+  end
 end
--- rspec_sample/spec/models/article_spec.rb
+++ rspec_sample/spec/models/article_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) {p "毎回の前準備"}
+  before(:all) {p "最初の一回のみの前準備"}
+  after(:each) {p "毎回の後処理"}
+  after(:all) {p "一番最後の後処理"}
+
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '.abbreviated_title' do
+    context '記事タイトルが20文字未満の場合' do
+      it '記事のタイトルがそのままのこと' do
+        article = Article.new(title: 'そのままのタイトル')
+        expect(article.abbreviated_title).to eq 'そのままのタイトル'
+      end
+    end
+    context '記事タイトルが20以上の時' do
+      it '記事が省略されることを確認' do
+        article = Article.new(title: 'a' * 20)
+        expect(article.abbreviated_title).to eq "#{'a' * 19}..."
+      end
+    end
+  end
+
+  describe '.published' do
+    before(:each) {p "テストグループ内の前準備"}
+    context '記事が未公開の場合' do
+      it '記事が公開状態になっているか' do
+        article = Article.new(status: :draft)
+        article.publish
+        expect(article.published?).to be_truthy
+      end
+    end
+  end
+  context '記事が公開されている場合' do
+    it '記事が公開状態のままであること' do
+      article = Article.new(status: :published)
+      article.publish
+      expect(article.published?).to be_truthy
+    end
+  end
+
+  it 'pendingの振る舞いチェク' do
+    pending "テストを保留します"
+    expect(1).to eq 2
+  end
+
+  skip '全体をスキップ' do
+    it 'skip振る舞いテスト' do
+      expect(1).to ep 2
+    end
+  end
 end
