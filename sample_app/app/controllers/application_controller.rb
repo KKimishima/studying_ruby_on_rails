c3437927f04471831946829a47738bd0b94c8abf
君島克弥 <K.Kimishima123@gmail.com>
2018-05-27 18:36:02 +0900

    <修正>  protect_from_forgeryの記述が間違っていたので修正

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgeryを有効に設定
-  protect_from_forgery with :exception, prepend: true
+  protect_from_forgery with: :exception, prepend: true
 end
