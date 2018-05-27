670515ed7de6ffbcad8f412c2f69ff78a2dab224
君島克弥 <K.Kimishima123@gmail.com>
2018-05-27 19:50:33 +0900

    helperの修正とテスト作成

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-  APP_NAME = 'Rails サンプルアプリ'.freeze
+  APP_NAME = 'Rails Sample App'
+
   def page_title
     base_title = APP_NAME
     return base_title if @title.blank?
-      `#{base_title}` | `#{@title}`
+    "#{base_title} | #{@title}"
   end
 end
