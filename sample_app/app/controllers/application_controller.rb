6b42fbd88ed04fb9c73725ac67842ace6f3c4271
君島克弥 <K.Kimishima123@gmail.com>
2018-05-27 18:03:40 +0900

    <追加>rubycops設定

--- sample_app/app/controllers/application_controller.rb
+++ sample_app/app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   # protect_from_forgeryを有効に設定
   protect_from_forgery with :exception, prepend: true
--- sample_app/config/application.rb
+++ sample_app/config/application.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -33,7 +35,7 @@ module SampleApp
     config.read_encrypted_secrets = true
 
     #  ジェネレートコマンドでの除外設定追加
-    config.generators  do |g|
+    config.generators do |g|
       g.skip_routes true
       g.stylesheets false
       g.javascripts false
